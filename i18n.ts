181ae92004a5825eb57bbef12370c8f3de65c7c2
IsmailGa <Gaismail777@gmail.com>
2025-03-18 02:52:01 +0500

    fixes with navigation

--- i18n.ts
+++ i18n.ts
@@ -1,10 +1,14 @@
 import { getRequestConfig } from "next-intl/server";
-import { notFound } from "next/navigation";
-import { locales } from "./config";
+import { redirect } from "next/navigation";
+import { locales, defaultLocale } from "./config";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const locale = await requestLocale;
-  if (!locales.includes(locale as any)) return notFound();
+
+  // Если локаль отсутствует или не поддерживается, перенаправляем на главную страницу
+  if (!locale || !locales.includes(locale as any)) {
+    redirect(`/${defaultLocale}`);
+  }
 
   return {
     messages: (await import(`@/locales/${locale}.json`)).default,
