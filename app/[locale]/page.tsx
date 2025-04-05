c09f05d87f829cd0f0048957d3a928b16b801cf0
Yusuf <safarov.1513@gmail.com>
2025-04-06 01:23:00 +0500

    -

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/container";
 import SectionOne from "./sections/1/SectionOne";
 import SectionTwo from "./sections/2/SectionOne";
 import SectionThree from "./sections/3/SectionThree";
-import SectionFour from "./sections/4/SectionFour";
+// import SectionFour from "./sections/4/SectionFour";
 import SectionFive from "./sections/5/SectionFive";
 import SectionSix from "./sections/6/SectionSix";
 import Navbar from "@/components/navbar";
@@ -19,7 +19,7 @@ export default function Home() {
       <Container>
         <SectionTwo />
         <SectionThree />
-        <SectionFour />
+        {/* <SectionFour /> */}
         <SectionFive />
         <SectionSix />
       </Container>
